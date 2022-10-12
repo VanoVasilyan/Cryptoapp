71191f9fd4221ac043740d436e8c7e9096fd2293
Vano <vanovasilyan@gmail.com>
2022-10-12 18:32:21 +0400

    add packages and Navbar component

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,8 +1,18 @@
+import { Routes, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
 
 const App = () => {
   return (
-    <div>
+    <div className='app'>
+      <div className="navbar">
+        
+      </div>
+      <div className="main">
 
+      </div>
+      <div className="footer">
+
+      </div>
     </div>
   );
 }
