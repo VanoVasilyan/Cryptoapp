62eee05868457ea072530d0b9b61952a36b310c2
Vano <vanovasilyan@gmail.com>
2022-10-13 18:27:33 +0400

    create footer and add antd styles

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -20,9 +20,17 @@ const App = () => {
             <Route path='/news' element={<News />} />
           </Routes>
         </Layout>
-      </div>
-      <div className="footer">
-
+        <div className="footer">
+          <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+            Cryptoverse <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to='/'>Home</Link>
+            <Link to='/exchanges'>Exchanges</Link>
+            <Link to='/news'>News</Link>
+          </Space>
+        </div>
       </div>
     </div>
   );
