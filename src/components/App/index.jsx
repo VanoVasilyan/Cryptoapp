03cde20d4ddfe73da6ac93eb7e0902840283f29d
Vano <vanovasilyan@gmail.com>
2022-10-13 18:15:50 +0400

    create main components

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 
-import { Navbar } from './../../components';
+import { Navbar, Home, Exchanges, Cryptocurrencies, CryptoDetails, News } from './../../components';
 import './App.css';
 
 const App = () => {
@@ -11,7 +11,15 @@ const App = () => {
         <Navbar />
       </div>
       <div className="main">
-
+        <Layout>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/exchanges' element={<Exchanges />} />
+            <Route path='/cryptocurrencies' element={<Cryptocurrencies />} />
+            <Route path='/crypto/:coinId' element={<CryptoDetails />} />
+            <Route path='/news' element={<News />} />
+          </Routes>
+        </Layout>
       </div>
       <div className="footer">
 
