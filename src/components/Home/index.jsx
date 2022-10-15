fcb2ef566ba82b48ef41cbd2eb93b37e9ecca4fb
Vano <vanovasilyan@gmail.com>
2022-10-15 18:11:56 +0400

    build Home component and fetch cryptocurrencies

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -6,13 +6,13 @@ import { useGetCryptosQuery } from '../../services/cryptoApi';
 import { Cryptocurrencies, News } from '../../components';
 
 const Home = () => {
-  const { data, isFetching } = useGetCryptosQuery();
+  const { data, isFetching } = useGetCryptosQuery(10);
   const globalStats = data?.data?.stats;
 
   if (isFetching) return "Loading..."
 
   return (
-    <>
+    <div className='routes'>
       <Typography.Title level={2} className="heading">Global Crypto Stats</Typography.Title>
       <Row>
         <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
@@ -25,13 +25,13 @@ const Home = () => {
         <Typography.Title level={2} className="home-title">Top 10 Cryptocurrencies in the world</Typography.Title>
         <Typography.Title level={3} className="show-more"><Link to='/cryptocurrencies'>Show More</Link></Typography.Title>
       </div>
-      <Cryptocurrencies />
+      <Cryptocurrencies simplifield/>
       <div className='home-heading-container'>
         <Typography.Title level={2} className="home-title">Latest Crypto News</Typography.Title>
-        <Typography.Title level={3} className="show-more"><Link to='/cryptocurrencies'>Show More</Link></Typography.Title>
+        <Typography.Title level={3} className="show-more"><Link to='/news'>Show More</Link></Typography.Title>
       </div>
-      <News />
-    </>
+      <News simplifield/>
+    </div>
   )
 }
 
