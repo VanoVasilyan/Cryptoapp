49e40c7f65541d9e2fa9cd55c896cff73b61ea30
Vano <vanovasilyan@gmail.com>
2022-10-16 17:10:29 +0400

    build select of options for news

--- src/components/News/index.jsx
+++ src/components/News/index.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import { useState } from 'react';
 import { Select, Typography, Row, Col, Avatar, Card } from 'antd';
 import moment from 'moment';
 import { useGetCryptoNewsQuery } from '../../services/cryptoNewsApi';
+import { useGetCryptosQuery } from '../../services/cryptoApi';
 
 const { Text, Title } = Typography;
 const { Option } = Select;
@@ -9,8 +10,10 @@ const { Option } = Select;
 const demoImage = 'https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News';
 
 const News = ({ simplifield }) => {
-    const count = simplifield ? 6 : 12
-    const { data: cryptoNews, isFetching } = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrency', count });
+    const [newsCategory, setNewsCategory] = useState('Cryptocurrency');
+    const count = simplifield ? 6 : 12;
+    const { data: cryptoNews, isFetching } = useGetCryptoNewsQuery({ newsCategory, count });
+    const { data } = useGetCryptosQuery(100);
 
     const newCryptoNewsArrayWithIds = cryptoNews?.value?.map((news, idx) => ({ ...news, id: idx + 1 }));
 
@@ -18,6 +21,27 @@ const News = ({ simplifield }) => {
 
     return (
         <Row gutter={[24, 24]} className='routes'>
+            {
+                !simplifield && (
+                    <Col span={24}>
+                        <Select
+                            showSearch
+                            className='select-news'
+                            placeholder='Select a Crypto'
+                            optionFilterProp='children'
+                            onChange={(value) => setNewsCategory(value)}
+                            filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLocaleLowerCase()) >= 0}
+                        >
+                            <Option value="Cryptocurrency">Cryptocurrency</Option>
+                            {
+                                data?.data?.coins.map((coin) => {
+                                    return <Option key={coin.uuid} value={coin.name}>{coin.name}</Option>
+                                })
+                            }
+                        </Select>
+                    </Col>
+                )
+            }
             {
                 newCryptoNewsArrayWithIds?.map((news) => {
                     return <Col xs={24} sm={8} lg={8} key={news.id}>
@@ -29,9 +53,16 @@ const News = ({ simplifield }) => {
                                 </div>
                                 <p>
                                     {
-                                        news.description.length > 100 ? `${news.description.substring(0, 100)}...` : news.description
+                                        news.description.length < 100 ? `${news.description.substring(0, 100)}...` : news.description
                                     }
                                 </p>
+                                <div className="provider-container">
+                                    <div>
+                                        <Avatar src={news.provider[0]?.image?.thumbnail?.contentUrl || demoImage} alt="news" />
+                                        <Text className="provider-name">{news.provider[0]?.name}</Text>
+                                    </div>
+                                    <Text>{moment(news.datePublished).startOf('ss').fromNow()}</Text>
+                                </div>
                             </a>
                         </Card>
                     </Col>
